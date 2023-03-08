49a097ddd55081023a3df1e8170ebcf8637e5d2d
Mr.Youngman <ali.javanmard14@gmail.com>
2023-03-08 10:23:26 +0330

    merge

--- CookProjectFrontend/src/Components/Foods/FoodDetail.jsx
+++ CookProjectFrontend/src/Components/Foods/FoodDetail.jsx
@@ -4,87 +4,321 @@ import element from "../../images/nodel-ba-panir.jpg";
 import author from "../../images/author.jpg";
 import Rating from "@mui/material/Rating";
 import PageBanner from "../Banner/PageBanner.js";
+import chief_profile_img from "../../images/chief_profile.jpg";
+import author_avatar from "../../images/author-avatar.png";
 import axios from "axios";
-import {BASE_API} from "../../App";
+import {BASE_API, check_error} from "../../App";
+import Swal from 'sweetalert2';
 
-function FoodDetail() {
-  const [food, setFood] = useState({});
-  const { food_id } = useParams();
+import {
+  ChiefCard,
+  ChiefContainer,
+  ChiefH1,
+  ChiefH2,
+  ChiefImage,
+  ChiefP,
+  ChiefWrapper,
+} from "./ForumStyle";
+import Sidebar from "../Sidebar";
+import Navbar from "../Navbar-second";
+import Grid from "@mui/material/Grid";
+import { useForm } from "react-hook-form";
+import { updateLogin, useLogin } from "../../hooks/useLogin";
+
+function ForumDetail() {
+  const [Forum, setForum] = useState({});
+  const [followedForum, setFollowedForum] = useState(false);
+  const [followedChief, setFollowedChief] = useState(false);
+  const { register, handleSubmit } = useForm();
+  const [chiefDetail, setChiefDetail] = useState({});
+  const { chiefName, forumName, forumId } = useParams();
+  const user = useLogin();
+  const username = user.username;
+  const Swal = require('sweetalert2')
+
   useEffect(() => {
-    const fetchData = async () => {
+    const checkFollowedForum = async () => {
       await axios
-        .get(BASE_API + "recipesbyid/",{
+        .get(BASE_API + `forums/${forumId}/checkFollowed`,{
             params: {
-              recipe_id: food_id
+              username: username
             },
-            headers: {
-              "Content-Type": "application/json",
+              headers: {
+                "Content-Type": "application/json",
             },
           }
         )
         .then((response) => {
-          setFood(response.data); //results
+          console.log(response.data);
+          if (response.data == "True")
+            setFollowedForum(true); //results
+          else
+            setFollowedForum(false); //results
         })
         .catch((error) => {
-          console.log("error in get recipe info");
+          console.log("error in get checking followed forum or not.");
         });
     }
+
+    const checkFollowedChief = async () => {
+      await axios
+        .get(BASE_API + `accounts/${chiefName}/hasFollowed`,{
+            params: {
+              username: username
+            },
+              headers: {
+                "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response.data);
+          if (response.data == "True")
+            setFollowedChief(true); //results
+          else
+            setFollowedChief(false); //results
+        })
+        .catch((error) => {
+          console.log("error in get checking followed forum or not.");
+        });
+    }
+
+    const fetchData = async () => {
+      await axios
+        .get(BASE_API + "forums/view",{
+            params: {
+              forumId: 1,
+              username: username
+            },
+              headers: {
+                "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response.data);
+          setForum(response.data); //results
+        })
+        .catch((error) => {
+          console.log("error in get forum info.");
+        });
+    }
+    checkFollowedForum();
+    checkFollowedChief();
     fetchData();
   }, []);
   
-  while(food.chief === undefined){
+  function follow_chief() {
+    axios
+        .put(BASE_API + `accounts/${chiefName}/follow/?username=${username}`,{
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        ).then((response)  => {
+          console.log(response.data);
+          setFollowedChief(true); //
+          Swal.fire({
+            title: 'Follow Chief',
+            text: `You follow chief ${chiefName} now`,
+            icon: 'success',
+          })
+        })
+        .catch((error) => {
+          console.log("error in follow chief.");
+        });
+  }
+
+  function unfollow_chief() {
+    axios
+        .delete(BASE_API + `accounts/${chiefName}/unfollow/?username=${username}`,{
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        ).then((response)  => {
+          console.log(response.data)
+          setFollowedChief(false); //results
+          Swal.fire({
+            title: 'Unfollow Chief',
+            text: `You unfollow chief ${chiefName}`,
+            icon: 'success',
+          })
+        })
+        .catch((error) => {
+          console.log("error in follow chief.");
+        });
+  }  
+  
+  function followForum() {
+    axios
+        .patch(BASE_API + `forums/${forumId}/join/?username=${username}`,{
+              headers: {
+                "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response.data);
+          setFollowedForum(true); //results
+          Swal.fire({
+            title: 'Follow Forum',
+            text: `You follow forum ${forumId} now`,
+            icon: 'success',
+          })
+        })
+        .catch((error) => {
+          console.log("error in get forum info.");
+        });
+  }
+
+  function unfollowForum() {
+    axios
+        .patch(BASE_API + `forums/${forumId}/leave/?username=${username}`,{
+              headers: {
+                "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response.data);
+          setFollowedForum(false); //results
+          Swal.fire({
+            title: 'Unfollow Forum',
+            text: `You unfollow forum ${forumId}`,
+            icon: 'success',
+          })
+        })
+        .catch((error) => {
+          console.log("error in get forum info.");
+        });
+  }
+
+  const onSubmit = (data) => {
+    console.log("on submit text:");
+    console.log(data)
+    axios
+        .post(
+            BASE_API + `forums/${forumId}/post/`,
+            {
+              params: {
+                sender: username,
+                text: data.text
+              },
+              headers: {
+                  "Content-Type": "application/json",
+              },
+            }
+        )
+        .then((response) => {
+            if (check_error(response.data))
+                alert(response.data.err_msg)
+            else{
+              Swal.fire({
+                title: 'Post Comment',
+                text: `You post a comment now`,
+                icon: 'success',
+              })
+              window.location.reload(false);
+            }
+        })
+        .catch((error) => {
+        });
+  };
+
+  while (Forum.messages == undefined){
     return <>Still loading...</>;
   }
 
   return (
     <>
-    <PageBanner pageName={"طرز پخت"} />
-    <div className="food-detail container">
-      <div className="detail-body">
-        <div className="food-details-content">
-          <h2 className="card-title">
-            {food.name}
-            <span className="release-year"> برای 4 نفر</span>
-          </h2>
-          <div className="author">
-            <img src={author} alt="author" />
-            <h6>{food.chief.first_name} {food.chief.last_name}</h6>
-          </div>
-          <div className="rating">
-            <Rating name="read-only" value={food.rate} readOnly />
-            <span className="price">{food.price}</span>
-          </div>
-          <div className="detail-description">
-            <p className="card-description">
-              {" "}
-              {food.description}
-            </p>
-          </div>
-          {/* <ul className="category-tags pt-10 pb-5">
-            <li>
-              <b>دسته بندی</b>
-              <span>:</span>
-              <a href="#">Book</a>
-              <a href="#">Cover</a>
-              <a href="#">Course</a>
-            </li>
-            <li>
-              <b>تگ ها</b>
-              <span>:</span>
-              <a href="#">Learn</a>
-              <a href="#">Teach</a>
-              <a href="#">Videos</a>
-            </li>
-          </ul> */}
-        </div>
-        <div className="images preview-images">
-          <img className="detail-img" src={element} alt="" />
-        </div>
-      </div>
-    
-    </div>
+      <Sidebar />
+      <Navbar />
+      <Grid container spacing={2}>
+        <Grid container item spacing={2} xs={12} sm={12} md={3}>
+          <Grid item xs={12} sm={12} md={12}>
+            <ChiefContainer>
+              <ChiefH1>پروفایل آشپز {chiefDetail.username}</ChiefH1>
+              <ChiefWrapper>
+                <ChiefCard>
+                  <ChiefImage src={chief_profile_img}></ChiefImage>
+                  <ChiefH2>
+                    {chiefDetail.first_name} {chiefDetail.last_name}
+                  </ChiefH2>
+                  <ChiefP>{chiefDetail.bio}</ChiefP>
+
+                  <a
+                    href={
+                      "mailto: " +
+                      chiefDetail.email +
+                      "?subject=Mail from Cookommunity"
+                    }
+                  >
+                    سوالات خود را بپرسید!
+                  </a>
+                  <button class="mbtn" hidden={followedChief} onClick={follow_chief}>دنبال کن !</button>
+                  <button class="mbtn2" hidden={!followedChief} onClick={unfollow_chief}>دنبال نکن :(</button>
+                </ChiefCard>
+              </ChiefWrapper>
+            </ChiefContainer>
+          </Grid>
+        </Grid>
+        <Grid container item spacing={2} xs={12} sm={12} md={9}>
+          <Grid item xs={12} sm={12} md={12}>
+            <div className="forum_body_area">
+              <div className="f-p-container">
+                <h1 className="f-title">فوروم {forumId}</h1>
+                <button class="mbtn" hidden={followedForum} onClick={followForum}>دنبال کن !</button>
+                <button class="mbtn2" hidden={!followedForum} onClick={unfollowForum}>دنبال نکن :(</button>
+              </div>
+              <div class="forum-post-top">
+                <a class="author-avatar" href="#">
+                  <img src={author_avatar} alt="author avatar" />
+                </a>
+                <div class="forum-post-author">
+                  
+                  <div class="comment-content">
+                    {Forum.messages.map((comment) => (
+                      <Grid>
+                        <a class="author-name" href="#">
+                          {comment.sender}
+                        </a>
+                        <div class="forum-author-meta">
+                          <div class="author-badge">
+                            <span>Conversation Starter</span>
+                          </div>
+                          <div class="author-badge">
+                            <i class="icon_calendar"></i>
+                            <p>{comment.date}</p>
+                          </div>
+                        </div>
+                        <p>
+                          {comment.text}
+                        </p>
+                      </Grid>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <form id="review-form" className="review-form" onSubmit={handleSubmit(onSubmit)}>
+                <h4>نظر خود را بنویسید</h4>
+                <br />
+                <input
+                  type="text"
+                  placeholder="متن"
+                  className="form-control"
+                  {...register("text")}
+                />
+                <button class="btn action_btn thm_btn" type="submit">
+                  ثبت نظر
+                </button>
+              </form>
+            </div>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 }
 
-export default FoodDetail;
+export default ForumDetail;
